7068e6bd548b8b6800e216a5d197eaf21593c69c
rbertoli <roberto.bertolino@gmail.com>
2016-01-04 15:24:07 +0100

    Change for running on Wildfly 9

---
+++ dcm4chee-arc-store/src/main/java/org/dcm4chee/archive/store/impl/PatientRegistration.java
@@ -0,0 +1,31 @@
+package org.dcm4chee.archive.store.impl;
+
+import java.util.logging.Logger;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import org.dcm4chee.archive.entity.Patient;
+
+/**
+ * 
+ * @author Roberto Bertolino <roberto.bertolino@gmail.com>
+ *
+ */
+
+//The @Stateless annotation eliminates the need for manual transaction demarcation
+@Stateless
+public class PatientRegistration {
+	
+	@Inject
+    private EntityManager em;
+	
+    @Inject
+    private Logger log;
+	
+	public void register(Patient patient) throws Exception {
+		log.info("Registering: " + patient);
+        em.persist(patient);        
+    }
+}
